55419373e398b8668d266fa5429b512aaafaaa53
Reelect <rejaealsun@gist.ac.kr>
2024-01-04 21:28:11 +0900

    feat(index.js,-package.json): env value, server start dependency

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
-//
+// check port environment variable
+if (!process.env.PORT) {
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
+}
+
+// get port environment variable
+const PORT = process.env.PORT;
+
 // Registers a HTTP GET route.
 // ref: https://betterprogramming.pub/video-stream-with-node-js-and-html5-320b3191a6b6
-//
-app.get("/", (req, res) => {
+app.get("/video", (req, res) => {
   const path = "./videos/SampleVideo_1280x720_1mb.mp4";
   fs.stat(path, (err, stats) => {
     if (err) {
@@ -25,11 +33,9 @@ app.get("/", (req, res) => {
   });
 });
 
-//
 // Starts the HTTP server.
-//
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(
-    `First example app listening on port ${port}, point your browser at http://localhost:3000`
+    `Microservice listening on port ${PORT}, point your browser at http://localhost:${PORT}/video`
   );
 });

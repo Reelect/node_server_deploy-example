2a158b39ac216b0fd485286e5f62f75afb9b6437
Reelect <rejaealsun@gist.ac.kr>
2024-01-12 15:48:32 +0900

    feat(DB): mongoDB apply for video indexing

--- minivideo/src/index.js
+++ minivideo/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const mongodb = require("mongodb");
 
 const app = express();
 
@@ -22,34 +23,82 @@ if (!process.env.VIDEO_STORAGE_PORT) {
   );
 }
 
+if (!process.env.DBHOST) {
+  throw new Error(
+    "Please specify the databse host using environment variable DBHOST."
+  );
+}
+
+if (!process.env.DBNAME) {
+  throw new Error(
+    "Please specify the name of the database using environment variable DBNAME"
+  );
+}
+
 // get environment variable
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
+
 console.log(
   `Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`
 );
 
-// Registers a HTTP GET route.
-app.get("/video", (req, res) => {
-  const forwardRequest = http.request(
-    {
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: "/video?path=changmo.mp4",
-      method: "GET",
-      headers: req.headers,
-    },
-    (forwardResponse) => {
-      res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-      forwardResponse.pipe(res);
-    }
-  );
+function main() {
+  return mongodb.MongoClient.connect(DBHOST).then((client) => {
+    const db = client.db(DBNAME);
+    const videosCollection = db.collection("videos");
 
-  req.pipe(forwardRequest);
-});
+    // Registers a HTTP GET route.
+    app.get("/video", (req, res) => {
+      const videoId = new mongodb.ObjectID(req.query.id);
+      videosCollection
+        .findOne({ _id: videoId })
+        .then((videoRecord) => {
+          if (!videoRecord) {
+            res.sendStatus(404);
+            return;
+          }
 
-// Starts the HTTP server.
-app.listen(PORT, () => {
-  console.log(`Microservice online`);
-});
+          const forwardRequest = http.request(
+            {
+              host: VIDEO_STORAGE_HOST,
+              port: VIDEO_STORAGE_PORT,
+              path: `/video?path=${videoRecord.videoPath}`,
+              method: "GET",
+              headers: req.headers,
+            },
+            (forwardResponse) => {
+              res.writeHeader(
+                forwardResponse.statusCode,
+                forwardResponse.headers
+              );
+              forwardResponse.pipe(res);
+            }
+          );
+          req.pipe(forwardRequest);
+        })
+        .catch((err) => {
+          console.error("Database query failed.");
+          console.error((err && err.stack) || err);
+          res.sendStatus(500);
+        });
+    });
+
+    // Starts the HTTP server.
+    app.listen(PORT, () => {
+      console.log(
+        `Microservice listening, please load the data file db-fixture/videos.json into your database before testing this microservice.`
+      );
+    });
+  });
+}
+
+main()
+  .then(() => console.log("Center Microservice online"))
+  .catch((err) => {
+    console.error("Microservice failed to start.");
+    console.error((err && err.stack) || err);
+  });
